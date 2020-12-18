e43ef2ab0153ab8282dffdb19e52ad7c7433f887
EricHier <developer.erichier@gmail.com>
2020-12-18 21:00:45 +0100

    added user details reference to account.js

--- models/account.js
+++ models/account.js
@@ -3,6 +3,6 @@ import ObjectId from "mongoose/lib/schema/objectid";
 
 export const accountModel = mongoose.model("account", {
     _id: { type: ObjectId, required: true, auto: true},
-    user: { type: String, required: true },
+    user: { type: String, required: true, ref: "user-detail" },
     balance: { type: Number, required: true, default: 0 },
 });
